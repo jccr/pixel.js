ce32de6872a9cdf5d0ab5dd9a626d91f1384ab04
Juan Carlos Corona Romero <c@rlos.email>
2020-02-17 00:10:47 -0800

    refactor to use puppeteer
    since playwright has bugs (that puppeteer also suffers from) with no
    workarounds

--- pixel.js
+++ pixel.js
@@ -1,66 +1,106 @@
 const process = require('process')
-const path = require('path')
-const fsp = require('fs').promises
-const playwright = require('playwright')
+const puppeteer = require('puppeteer')
+const iPhone = puppeteer.devices['iPhone SE']
 
-const iPhoneSE = playwright.devices['iPhone SE']
+const args = process.argv.slice(2)
 
-const args = process.argv.slice(2);
+const escapeXpathString = str => {
+  const splitQuotes = str.replace(/'/g, `', "'", '`)
+  return `concat('${splitQuotes}', '')`
+}
 
-; (async () => {
-  const browser = await playwright[args[0]].launch({ headless: true });
-  const context = await browser.newContext({
-    viewport: iPhoneSE.viewport,
-    userAgent: iPhoneSE.userAgent
-  })
-  const page = await context.newPage()
+const clickByText = async (page, text, tag = '*') => {
+  const escapedText = escapeXpathString(text)
+  const linkHandlers = await page.$x(
+    `//${tag}[contains(text(), ${escapedText})]`,
+  )
+
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click()
+  } else {
+    throw new Error(`Link not found: ${text}`)
+  }
+}
+
+let browser
+
+async function main() {
+  browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.setBypassCSP(true)
+  await page.emulate(iPhone)
 
   console.log('Navigate to login')
   await page.goto('https://instagram.com/accounts/login/')
 
+  await page.waitFor('input[name=username]')
+
   console.log('Type username')
-  await page.type("input[name=username]", args[1])
+  await page.type('input[name=username]', args[0])
 
   console.log('Type password')
-  await page.type("input[name=password]", args[2])
+  await page.type('input[name=password]', args[1])
 
-  console.log('Click login')
-  await page.click('text="Log In"')
+  console.log('Click log in')
+  await clickByText(page, 'Log In')
 
   console.log('Wait for navigation')
-  await page.waitForNavigation()
+  try {
+    await page.waitForNavigation()
+  } catch (error) {
+    console.error(error)
+    throw new Error('Log in error/timeout: Wrong credentials maybe?')
+  }
 
   console.log('Go to root')
-  await page.goto('https://instagram.com')
+  await page.goto('https://instagram.com', { waitUntil: 'networkidle2' })
 
-  console.log('Wait 3s')
-  await page.waitFor(3)
-
-  page.on('filechooser', () => { })
+  console.log('Try to suppress any popup dialogs')
+  try {
+    await page.click('[role=dialog] button:nth-child(2)')
+    await page.waitFor(1000)
+  } catch (_) {}
+  try {
+    await page.click('[role=dialog] button:nth-child(2)')
+    await page.waitFor(1000)
+  } catch (_) {}
+  try {
+    await page.click('[role=dialog] button:nth-child(2)')
+    await page.waitFor(1000)
+  } catch (_) {}
 
   console.log('Click new post')
-  await page.click('[data-testid="new-post-button"]')
+  page.waitFor('[data-testid="new-post-button"]')
 
-  const input = await page.$('input[type="file"]')
-  await input.setInputFiles({
-    name: 'photo.jpg',
-    type: 'image/jpeg',
-    data: await fsp.readFile(args[3], 'base64')
-  })
+  const [fileChooser] = await Promise.all([
+    page.waitForFileChooser(),
+    page.click('[data-testid="new-post-button"]'),
+  ])
+  console.log('Place photo')
+  await fileChooser.accept([args[2]])
 
   console.log('Wait for photo preview')
   await page.waitFor('[style*="blob:https://www.instagram.com"]')
 
-  await page.screenshot({ path: 'debug/next.png' })
-
   console.log('Click next')
-  await page.click('text="Next"')
+  await clickByText(page, 'Next', 'button')
 
-  console.log('Wait for upload preview')
+  console.log('Wait for post preview')
   await page.waitFor('[alt="Preview of photo to be uploaded"]')
 
-  await page.screenshot({ path: 'debug/share.png' })
-
   console.log('Click share')
-  await page.click('text="Share"')
-})()
+  await clickByText(page, 'Share', 'button')
+
+  console.log('Wait for navigation')
+  await page.waitForNavigation()
+
+  console.log('Done!')
+  await browser.close()
+}
+
+main().catch(async error => {
+  console.error(error)
+  await browser.close()
+  process.exit(1)
+})
